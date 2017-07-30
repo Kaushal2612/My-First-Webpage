32e932139139186f7fa8fe325cbc3441061ccc9a
Kaushal Jhawar <kaushaljhawar1996@gmail.com>
2017-07-30 23:43:56 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,38 @@
+﻿<html>
+<script type="text/javascript">
+<!--// Form validate code will come here.
+
+function validate()
+{
+
+	if( document.feedback.Firstname.value == "" )
+	
+	{
+	alert( "Please provide your name!" );
+	document.feedback.Firstname.focus() ;
+	return false;
+	}
+	
+	if( document..Lastname.value == "" )
+	
+	{
+	alert( "Please provide your name!" );
+	document.feedback.Lastname.focus() ;
+	return false;
+	}
+	
+	if( document.feedback.Comment.value == "" )
+	
+	{
+	alert( "Please give ur Comment!" );
+	document.feedback.Comment.focus() ;
+	return false;
+	}
+	
+	
+	return( true );
+}
+
+//-->
+</script>
+</html>
